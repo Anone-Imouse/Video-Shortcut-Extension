923e2cdf9502d52b7749d907aee2cbfa3459c552
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2021-06-15 13:30:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,5 @@
 const {log} = console;
 
-if(document.querySelector('video')){
-    if(document.querySelector('video').autoplay){
-        document.querySelector('video').pause()
-        document.querySelector('video').addEventListener('click',myFunc)
-        
-function myFunc(){
-document.querySelector('video').removeEventListener('click',myFunc)
-document.querySelector('video').play()
-  }
- }
-}
-
-if(document.querySelector('iframe')){
-    if(document.querySelector('iframe').src == "https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?&enablejsapi=1&autoplay=1&mute=1"){
-document.querySelector('iframe').remove()
-}
-}
-
-if(location.href.toLowerCase().includes('astley')){
-   document.body.innerHTML = `
-        <h1>NoNoNo!!!</h1>
-  <p>This page was bad and tried to rickroll you :(</p>
-    `
-}
-
-setInterval(() => {
-    
-if(location.href === 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' || location.href === 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtu.be'){
-    location.href = "about:blank"
-}
-},100)
-
-if(location.href.toLowerCase().indexOf('noordstar') > -1){
-     location.href = "about:blank"
-}
-
 document.addEventListener('keyup',handleKeyUp)
 
 function handleKeyUp(e){
