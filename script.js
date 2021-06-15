5b8f9c3618c52a217d59e4fbd64820ccac6ef326
Anone-Imouse <73369548+Anone-Imouse@users.noreply.github.com>
2021-06-15 13:19:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,85 @@
-window.addEventListener("load",function(){
-const rando=Math.floor(Math.random() * 5) + 1;
+const {log} = console;
 
-//console.log(rando)
-
-/*if(rando !== 5){
-   alert('Better luck next time :(');
-    
-    window.location.reload(true)
-}
-else if (rando === 5){
-    alert('Lucky Boi! :)')
-}
-else{
-    alert('Error')
-    for(let i=0; i<100; i++){
-        console.error('Error!!! :(')
-    }
-}*/
-
-
-    /*let sssssshhhhhhwhhhhhyyyyyyyyymyyyyggguuuuyyyyydude='0123456789ABCDEF';
-    document.querySelectorAll('*').forEach(t => t.style.outline=`1px solid #${sssssshhhhhhwhhhhhyyyyyyyyymyyyyggguuuuyyyyydude[Math.floor(Math.random() * 15)]}${sssssshhhhhhwhhhhhyyyyyyyyymyyyyggguuuuyyyyydude[Math.floor(Math.random() * 15)]}${sssssshhhhhhwhhhhhyyyyyyyyymyyyyggguuuuyyyyydude[Math.floor(Math.random() * 15)]}${sssssshhhhhhwhhhhhyyyyyyyyymyyyyggguuuuyyyyydude[Math.floor(Math.random() * 15)]}${sssssshhhhhhwhhhhhyyyyyyyyymyyyyggguuuuyyyyydude[Math.floor(Math.random() * 15)]}${sssssshhhhhhwhhhhhyyyyyyyyymyyyyggguuuuyyyyydude[Math.floor(Math.random() * 15)]}`)
-*/                                          
-
-    
-//if(document.querySelector('button.VfPpkd-LgbsSe')){ document.querySelector('button.VfPpkd-LgbsSe').click()}    
-    let var1=true;
-    let var2=true;
-    
-    document.addEventListener('keydown',function(e){
-        //console.log(e.code,e.which,e.key)
-if(e.key=='P' && document.querySelector('video') && e.ctrlKey){
-               //alert(asking)
-     if(var1===true){
-document.querySelector('video').requestPictureInPicture()
-     var1=false;
-    }
-    else if(var1===false){
-        var1=true;
-        document.exitPictureInPicture();
-    }
-  }
+if(document.querySelector('video')){
+    if(document.querySelector('video').autoplay){
+        document.querySelector('video').pause()
+        document.querySelector('video').addEventListener('click',myFunc)
         
-        else if(e.key=='F' && e.ctrlKey){
-            //alert(asking)
-        if(var2===true){
-         if(document.querySelector('video')){
-document.querySelector('video').requestFullscreen();
-        }
-         else if(document.querySelector('iframe')){ document.querySelector('iframe').requestFullscreen();
-        }
-         else if(document.querySelector('canvas')){ document.querySelector('canvas').requestFullscreen();
-        }
-          var2=false;
+function myFunc(){
+document.querySelector('video').removeEventListener('click',myFunc)
+document.querySelector('video').play()
+  }
+ }
+}
+
+if(document.querySelector('iframe')){
+    if(document.querySelector('iframe').src == "https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?&enablejsapi=1&autoplay=1&mute=1"){
+document.querySelector('iframe').remove()
+}
+}
+
+if(location.href.toLowerCase().includes('astley')){
+   document.body.innerHTML = `
+        <h1>NoNoNo!!!</h1>
+  <p>This page was bad and tried to rickroll you :(</p>
+    `
+}
+
+setInterval(() => {
+    
+if(location.href === 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' || location.href === 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&feature=youtu.be'){
+    location.href = "about:blank"
+}
+},100)
+
+if(location.href.toLowerCase().indexOf('noordstar') > -1){
+     location.href = "about:blank"
+}
+
+document.addEventListener('keyup',handleKeyUp)
+
+function handleKeyUp(e){
+if(e.key=='P' && document.querySelector('video') && e.ctrlKey){
+     if(!e.altKey){
+    if(!document.pictureInPictureElement){
+    for(let i = 0; i < document.querySelectorAll('video').length; i++){
+    if(!document.pictureInPictureElement){
+     document.querySelectorAll('video')[i].requestPictureInPicture()
     }
-      else if(var2===false){
-      var2=true;
-          document.exitFullscreen();
-      }
+    else{
+     null
     }
-    })
-})
+}
+        setTimeout(console.clear,3000)
+    }
+    else{
+     document.exitPictureInPicture();
+    }
+}   
+else{
+    if(!document.pictureInPictureElement){
+ let answer = +(prompt('What video do you want to be played in Picture-In-Picture?'))
+ 
+ document.querySelectorAll('video')[answer].requestPictureInPicture()
+    }
+    else{
+     document.exitPictureInPicture()
+    }
+}
+}
+    else if(e.key =='S' && e.ctrlKey){
+        function scroll(){
+            scrollBy(0,10)
+        }
+        let int = setInterval(scroll,100)
+        document.removeEventListener('keyup',handleKeyUp)
+        document.addEventListener('keyup',handleKeyUp2)
+        function handleKeyUp2(e){
+            if(e.key === 's'){
+                clearInterval(int)
+                document.addEventListener('keyup',handleKeyUp)
+                document.removeEventListener('keyup',handleKeyUp2)
+            }
+        }
+    }
+}
